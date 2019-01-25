83d677fa686bd7bd0d309f2cfa7b7dce02667c5a
Sergio Lopez <slp@sinrega.org>
2019-01-25 09:29:04 +0100

    Changes to run the session as an unprivileged user

--- build.sh
+++ build.sh
@@ -26,8 +26,10 @@ fi
 CTR=$($B from alpine:edge)
 $B copy $CTR files/repositories /etc/apk/repositories
 
-$B run $CTR apk add busybox-initscripts busybox xorg-server xf86-input-keyboard xf86-input-mouse xf86-input-libinput ttf-ubuntu-font-family i3wm dbus dbus-x11 xterm lightdm lightdm-gtk-greeter pulseaudio pulseaudio-alsa openrc libgudev udev-init-scripts eudev udev-init-scripts-openrc xrandr flatpak alsa-utils consolekit2
+$B run $CTR apk add busybox-initscripts busybox xorg-server xf86-input-keyboard xf86-input-mouse xf86-input-libinput ttf-ubuntu-font-family i3wm dbus dbus-x11 xterm lightdm lightdm-gtk-greeter pulseaudio pulseaudio-alsa openrc libgudev udev-init-scripts eudev udev-init-scripts-openrc xrandr flatpak alsa-utils consolekit2 sudo setxkbmap
 
+$B copy $CTR files/flatkvm-boot /etc/init.d/flatkvm-boot
+$B run $CTR rc-update add flatkvm-boot boot
 $B run $CTR rc-update add acpid default
 $B run $CTR rc-update add alsa default
 $B run $CTR rc-update add bootmisc boot
@@ -44,15 +46,19 @@ $B run $CTR rc-update add udev-postmount default
 $B run $CTR rc-update add udev-trigger sysinit
 $B run $CTR rc-update add urandom boot
 
+$B copy $CTR files/shadow /etc/shadow
+$B copy $CTR files/sudoers /etc/sudoers
+$B copy $CTR files/group /etc/group
 $B copy $CTR files/lightdm.conf /etc/lightdm/lightdm.conf
-$B run $CTR mkdir -p /root/.config/i3
-$B copy $CTR files/i3.config /root/.config/i3/config
-$B copy $CTR files/interfaces /etc/network/interfaces
+$B run $CTR mkdir -p /home/flatkvm/.config/i3
+$B copy $CTR files/i3.config /home/flatkvm/.config/i3/config
 $B run $CTR rm /etc/network/if-up.d/dad
+$B copy $CTR files/interfaces /etc/network/interfaces
 $B copy $CTR files/resolv.conf /etc/resolv.conf
 $B copy $CTR files/asound.state /var/lib/alsa/asound.state
-$B copy $CTR files/Xmodmap /root/.Xmodmap
+$B copy $CTR files/Xmodmap /home/flatkvm/.Xmodmap
 $B copy $CTR files/flatkvm-agent /usr/bin/flatkvm-agent
+$B run $CTR mkdir -p /var/lib/flatpak
 
 $B copy $CTR files/roboto.tgz /tmp/roboto.tgz
 $B run $CTR tar xpf /tmp/roboto.tgz -C /
